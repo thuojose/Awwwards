bf3dc500611d656e2699f4f1d29a81259cee8bd1
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-13 23:10:57 +0300

    adding of class model for project

--- awewards/models.py
+++ awewards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Projects(models.Model):
+    project_name = models.CharField(max_length=50, blank=True)
+    project_photo = CloudinaryField('projectpics/')
+    description = models.TextField(max_length=400,blank=True)
+    github_repo = models.CharField(max_length=150, blank=True)
+    url = models.CharField(max_length=60,blank=True)
+    uploader = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
