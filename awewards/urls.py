816cd1d5468cbfc558449dda290074d83a456a06
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-13 23:47:59 +0300

    added url for profile

--- awewards/urls.py
+++ awewards/urls.py
@@ -10,7 +10,7 @@ urlpatterns=[
     path('accounts/', include('django.contrib.auth.urls')),
     path('home/',views.home, name='home'),
     path('project/<int:project_id>[0-9]',views.project,name ='project'),
-     
+     path('profile/',views.profile, name='profile'),
 ]
 
 if settings.DEBUG:
