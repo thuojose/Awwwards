0cc5f69a1df9f1d802e149aab22299bbb3259cf9
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-13 22:42:36 +0300

    added function for signup/signin

--- awewards/views.py
+++ awewards/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', locals())
