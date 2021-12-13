8f9a7f424d0aa89982fa3873c52f82a88ebd18c7
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-13 23:43:58 +0300

    added class model for profile

--- awewards/models.py
+++ awewards/models.py
@@ -10,6 +10,27 @@ class Projects(models.Model):
     github_repo = models.CharField(max_length=150, blank=True)
     url = models.CharField(max_length=60,blank=True)
     uploader = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
+    
+    
+class Profile(models.Model):
+    profile_photo = CloudinaryField('profpics')
+    bio = models.TextField(blank = True)
+    user_id = models.OneToOneField(User, null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bio
+
+    def save_user(self):
+        self.save()
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
+
+    @receiver(post_save,sender=User)
+    def save_profile(sender,instance, **kwargs):
+        instance.profile.save()
 
     def __str__(self):
         return self.url
