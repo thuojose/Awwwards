5101038038b202eff5f376c16a91133b5f041eb5
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-13 23:03:03 +0300

    added media_url i settigs,py

--- awards/settings.py
+++ awards/settings.py
@@ -130,4 +130,9 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
+#configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
